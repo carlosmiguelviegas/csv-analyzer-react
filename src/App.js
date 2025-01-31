1b8efcec08145afd61ffafd09eaac10f969a8e32
unknown <carlosmviegas86@gmail.com>
2025-01-31 18:37:01 +0000

    added css styles to active class. await for the success response and check if status is 200

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ const App = () => {
     const files = Array.from(event['dataTransfer']['files']);
     const formData = new FormData();
     formData.append('file', files[0]);
-    await axios.post('http://localhost:8000/analyze', formData, { 'content-type': 'multipart/form-data' });
+    const { status } = await axios.post('http://localhost:8000/analyze', formData, { 'content-type': 'multipart/form-data' });
+    status === 200 ? ;
  };
 
   return (
     <section className={styles.container}>
-      <section className={styles.section `${active ? styles.active : ''}`} onDragOver={(e) => e.preventDefault()} onDrop={(e) => handlerOnDrop(e)}
+      <section className={`${styles.section} ${active ? styles.active : ''}`} onDragOver={(e) => e.preventDefault()} onDrop={(e) => handlerOnDrop(e)}
         onDragEnter={() => setActive(true)} onDragLeave={() => setActive(false)} >
       </section>
     </section>
