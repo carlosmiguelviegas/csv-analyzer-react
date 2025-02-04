442bd2a4376e708f10dcf9571a02c2dd5cd1cd15
unknown <carlosmviegas86@gmail.com>
2025-02-04 21:56:25 +0000

    added use state to track display property

--- src/components/navigation-bar/NavigationBar.js
+++ src/components/navigation-bar/NavigationBar.js
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import styles from './NavigationBar.module.css';
 
 const NavigationBar = () => {
 
+  const [ display, setDisplay ] = useState(false);
+
   return (
     <nav className={styles.bar}>
-      <button className={styles.barButton}>Log in</button>
+      <button className={styles.barButton} onClick={} >Log in</button>
     </nav>
   );
 
