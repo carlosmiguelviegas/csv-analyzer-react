fb14fc15d7a38edab001e8fae26eefe3ecc7891c
unknown <carlosmviegas86@gmail.com>
2025-02-04 21:52:52 +0000

    updated from section to nav

--- src/components/navigation-bar/NavigationBar.js
+++ src/components/navigation-bar/NavigationBar.js
@@ -3,9 +3,9 @@ import styles from './NavigationBar.module.css';
 const NavigationBar = () => {
 
   return (
-    <section className={styles.bar}>
+    <nav className={styles.bar}>
       <button className={styles.barButton}>Log in</button>
-    </section>
+    </nav>
   );
 
 };
