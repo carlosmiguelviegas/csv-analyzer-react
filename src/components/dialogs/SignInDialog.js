2cf5963f0aa07a853dd92bdcde2ae5ebdc7b5308
unknown <carlosmviegas86@gmail.com>
2025-02-05 22:38:16 +0000

    provided modal structure

--- src/components/dialogs/SignInDialog.js
+++ src/components/dialogs/SignInDialog.js
@@ -3,9 +3,18 @@ import styles from './SignInDialog.module.css';
 const SignInDialog = () => {
 
   return (
-    <section className={styles.modal}>
-      <p>sign in modal</p>
-    </section>
+    <div className={styles.modal}>
+      <section className={styles.container}>
+        <h2>Sign in</h2>
+        <form>
+          <label htmlFor=''>Email</label>
+          <input type='email' />
+          <label htmlFor=''>Password</label>
+          <input type='password' />
+          <button type='submit'>Sign in</button>
+        </form>
+      </section>
+    </div>
   );
 
 };
