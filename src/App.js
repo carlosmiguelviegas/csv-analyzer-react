9ac60ca56729f4800cafe069a4e6e23d14fe47e6
unknown <carlosmviegas86@gmail.com>
2025-02-18 17:46:45 +0000

    passed through props the setIsSignedIn function to the nav bar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
   return (
     <>
-      <NavigationBar />
+      <NavigationBar setIsSignedIn={setIsSignedIn} />
       <Dashboard />
     </>
   );
--- src/components/navigation-bar/NavigationBar.js
+++ src/components/navigation-bar/NavigationBar.js
@@ -3,14 +3,14 @@ import { createPortal } from 'react-dom';
 import styles from './NavigationBar.module.css';
 import SignInDialog from '../dialogs/SignInDialog';
 
-const NavigationBar = () => {
+const NavigationBar = ({ setIsSignedIn }) => {
 
   const [ display, setDisplay ] = useState(false);
 
   return (
     <nav className={styles.bar}>
       <button className={styles.barButton} onClick={() => setDisplay(true)} >Log in</button>
-      {display && createPortal(<SignInDialog  setDisplay={setDisplay} />, document.body)}
+      {display && createPortal(<SignInDialog  setDisplay={setDisplay}  />, document.body)}
     </nav>
   );
 
