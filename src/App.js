a5467d209bdb35735ce46645b083f1f4b15ef586
unknown <carlosmviegas86@gmail.com>
2025-02-18 17:42:48 +0000

    created a state property in app component to track whether the user is logged in or not and display or hide the dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 import NavigationBar from './components/navigation-bar/NavigationBar';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
+
+  const [ isSignedIn, setIsSignedIn ] = useState(false);
 
   return (
     <>
